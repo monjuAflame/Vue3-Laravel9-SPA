5095ea3aeab597713c38a47452f5919f348bab21
monjuAflame <monjuaflame@gmail.com>
2022-09-05 11:08:26 +0600

    vue3 added and post crud ready also fetch table data

---
+++ resources/js/components/Posts/Index.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full">
+                        <thead class="bg-white border-b">
+                            <tr>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                >
+                                    #
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Title
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Content
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Created At
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="post in posts">
+                                <td
+                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
+                                >
+                                    {{ post.id }}
+                                </td>
+                                <td
+                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
+                                >
+                                    {{ post.title }}
+                                </td>
+                                <td
+                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
+                                >
+                                    {{ post.content }}
+                                </td>
+                                <td
+                                    class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900"
+                                >
+                                    {{ post.created_at }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    mounted() {
+        this.fetchPosts();
+    },
+    methods: {
+        fetchPosts() {
+            axios
+                .get("/api/posts")
+                .then((response) => (this.posts = response.data))
+                .catch((error) => console.log(error));
+        },
+    },
+};
+</script>
