6da332f83544b909d5e72155cfaad271ff7c21f8
monjuAflame <monjuaflame@gmail.com>
2022-09-06 18:48:57 +0600

    post create form

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -56,7 +56,7 @@
         <!-- Page Content -->
         <main>
             <div class="py-12">
-                <div class="w-full sm:px-6 lg:px-8">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div
                         class="bg-white overflow-hidden shadow-sm sm:rounded-lg"
                     >
