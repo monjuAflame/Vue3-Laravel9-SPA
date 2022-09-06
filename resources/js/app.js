2a240631ba6f5afbadfcfaee970c57f55819476a
monjuAflame <monjuaflame@gmail.com>
2022-09-06 17:58:27 +0600

    route cleanup and naming

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,12 @@
 import "./bootstrap";
 
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+
 import App from "./layouts/App.vue";
 import LaravelVuePagination from "laravel-vue-pagination";
-import PostsIndex from "./components/Posts/Index.vue";
-import PostsCreate from "./components/Posts/Create.vue";
-// import App from "./App.vue";
 
-const routes = [
-    { path: "/", component: PostsIndex },
-    { path: "/posts/create", component: PostsCreate },
-];
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+// import App from "./App.vue";
+import router from "./routes/index";
 
 const app = createApp(App);
 app.use(router);
